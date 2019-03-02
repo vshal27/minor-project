ecb91d8d182cce42640aa36e9a0d0b5ba25ca45a
vshal27 <43995084+vshal27@users.noreply.github.com>
2019-03-02 19:01:01 +0530

    Add files via upload

---
+++ p1.r
@@ -0,0 +1,12 @@
+# Name : Vishal Kumar Chowdhury
+# Roll : 1606080
+# Date Created: 13.2.2019
+
+n = readline("ENTER A NUMBER ")
+n = as.integer(n)
+
+if (n>=0 && n<=10 ){
+	print("okay")
+}else{
+	print("error")
+}
